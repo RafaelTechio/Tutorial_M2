3b9c5ece26b4192d1524faa6c6de75c93e1f49ca
TechioRafael <110608373+RafaelTechio@users.noreply.github.com>
2022-10-23 14:33:39 -0300

    FEAT: subindo .sql semana 2

---
+++ SEMANA_02/Entrega/bd.sql
@@ -0,0 +1,39 @@
+CREATE TABLE curriculo ( path_foto VARCHAR(255),
+nome VARCHAR(50),
+cargo VARCHAR(50),
+contato VARCHAR(50),
+descricao VARCHAR(255),
+id INT PRIMARY KEY );
+
+CREATE TABLE experiencia ( cargo VARCHAR(50),
+empresa VARCHAR(50),
+id INT PRIMARY KEY,
+descricao VARCHAR(255),
+inicio DATE,
+fim DATE,
+idcurriculo INT, FOREIGN KEY (idcurriculo) REFERENCES curriculo(id) );
+
+CREATE TABLE formacao ( curso VARCHAR(50),
+descricao VARCHAR(50),
+id INT PRIMARY KEY,
+inicio DATE,
+fim DATE,
+idcurriculo INT,
+FOREIGN KEY (idcurriculo) REFERENCES curriculo(id) );
+
+CREATE TABLE realizacao ( id INT PRIMARY KEY,
+nome VARCHAR(50),
+ano DATE,
+descricao VARCHAR(255),
+idcurriculo INT, FOREIGN KEY (idcurriculo) REFERENCES curriculo(id) );
+
+CREATE TABLE habilidade ( nome VARCHAR(50), id INT PRIMARY KEY,
+atual INT,
+maxima INT,
+idcurriculo INT, FOREIGN KEY (idcurriculo) REFERENCES curriculo(id) );
+
+CREATE TABLE personalidade ( nome VARCHAR(50),
+id INT PRIMARY KEY,
+atual INT,
+maxima INT,
+idcurriculo INT, FOREIGN KEY (idcurriculo) REFERENCES curriculo(id) );
