466223c5ec62a1e706f8e5858c237a5c36655840
TechioRafael <110608373+RafaelTechio@users.noreply.github.com>
2022-10-23 18:07:36 -0300

    FEAT: entrega semana 3

---
+++ SEMANA_03/Entrega/README.md
@@ -0,0 +1,22 @@
+## Entrega Semana 3
+## Banco de Dados
+Como a construção do currículo será progredida de semana a semana, criei o banco de dados na raíz do arquivo e farei as alterações nele, pois assim as próximas entregas acessam esse banco e não é preciso ficar criando novos iguais.
+## Rotas
+Como pedido na atividade, criei as rotas necessárias para resgatar as informações do banco de dados que criei. Essas rotas são:
+### Curriculo
+Retorna as informações base do curriculo e de todas as outras tabelas criadas, relacionadas com o currículo de ID 1
+### Habilidades
+/habilidades
+Retorna as linhas de habilidades relacionadas com o currículo de ID 1
+### Experiências
+/experiencias
+Retorna as linhas de experiências relacionadas com o currículo de ID 1
+### Formações
+/formações
+Retorna as linhas de formações relacionadas com o currículo de ID 1
+### Personalidades
+/personalidades
+Retorna as linhas de personalidades relacionadas com o currículo de ID 1
+### Realizações
+/realizações
+Retorna as linhas de realizações relacionadas com o currículo de ID 1
---
+++ SEMANA_03/Entrega/app.js
@@ -0,0 +1,121 @@
+const express = require('express'); 
+    const app = express();
+
+    const hostname = '127.0.0.1';
+    const port = 3000;
+    const sqlite3 = require('sqlite3').verbose();
+    const DBPATH = __dirname + '/../../db.db'; //use o nome que você achar melhor para o banco de dados
+    const db = new sqlite3.Database(DBPATH); // Abre o banco
+
+    app.use(express.json());
+
+    async function getCurriculo(id = 1){
+        const curriculo = await new Promise((resolve) => db.all(`SELECT * FROM curriculo WHERE id = ${id}`, [],  (err, rows ) => {
+            if (err) {
+                console.log(err.message)
+            }
+            resolve(rows[0])
+        }));
+
+        curriculo.experiencias = await getExperiencias(id)
+        curriculo.habilidades = await getHabilidades(id)
+        curriculo.formacoes = await getFormacoes(id)
+        curriculo.personalidades = await getPersonalidades(id)
+        curriculo.realizacoes = await getRealizacoes(id)
+
+        return curriculo
+    }
+
+    async function getHabilidades(idCurriculo = 1){
+        return await new Promise((resolve) => db.all(`SELECT * FROM habilidade WHERE idCurriculo = ${idCurriculo}`, [],  (err, rows ) => {
+            if (err) {
+                console.log(err.message)
+            }
+            resolve(rows)
+        }));
+    }
+    async function getExperiencias(idCurriculo = 1){
+        return await new Promise((resolve) => db.all(`SELECT * FROM experiencia WHERE idCurriculo = ${idCurriculo}`, [],  (err, rows ) => {
+            if (err) {
+                console.log(err.message)
+            }
+            resolve(rows)
+        }));
+    }
+    async function getFormacoes(idCurriculo = 1){
+        return await new Promise((resolve) => db.all(`SELECT * FROM formacao WHERE idCurriculo = ${idCurriculo}`, [],  (err, rows ) => {
+            if (err) {
+                console.log(err.message)
+            }
+            resolve(rows)
+        }));
+    }
+    async function getPersonalidades(idCurriculo = 1){
+        return await new Promise((resolve) => db.all(`SELECT * FROM personalidade WHERE idCurriculo = ${idCurriculo}`, [],  (err, rows ) => {
+            if (err) {
+                console.log(err.message)
+            }
+            resolve(rows)
+        }));
+    }
+    async function getRealizacoes(idCurriculo = 1){
+        return await new Promise((resolve) => db.all(`SELECT * FROM realizacao WHERE idCurriculo = ${idCurriculo}`, [],  (err, rows ) => {
+            if (err) {
+                console.log(err.message)
+            }
+            resolve(rows)
+        }));
+    }
+
+    app.get('/curriculo', async (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+
+        const curriculo = await getCurriculo(1)
+        res.json(curriculo)
+    });
+
+    app.get('/experiencias', async (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+
+        const experiencias = await getExperiencias(1)
+        res.json(experiencias)
+    });
+
+    app.get('/habilidades', async (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+
+        const habilidades = await getHabilidades(1)
+        res.json(habilidades)
+    });
+
+    app.get('/formacoes', async (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+
+        const formacoes = await getFormacoes(1)
+        res.json(formacoes)
+    });
+
+    app.get('/personalidades', async (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+
+        const personalidades = await getPersonalidades(1)
+        res.json(personalidades)
+    });
+
+    app.get('/realizacoes', async (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+
+        const realizacoes = await getRealizacoes(1)
+        res.json(realizacoes)
+    });
+
+/* Inicia o servidor */
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
