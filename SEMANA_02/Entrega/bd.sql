7afafc6389f345742f68f69ffb65e0f172226b2b
TechioRafael <110608373+RafaelTechio@users.noreply.github.com>
2022-10-23 14:46:29 -0300

    FIX: alterando tipo e adicionando auto increment nas chaves primarias

--- SEMANA_02/Entrega/bd.sql
+++ SEMANA_02/Entrega/bd.sql
@@ -5,11 +5,11 @@ telefone VARCHAR(50),
 endereco VARCHAR(50),
 email VARCHAR(50),
 descricao VARCHAR(255),
-id INT PRIMARY KEY );
+id INTEGER PRIMARY KEY AUTOINCREMENT);
 
 CREATE TABLE experiencia ( cargo VARCHAR(50),
 empresa VARCHAR(50),
-id INT PRIMARY KEY,
+id INTEGER PRIMARY KEY AUTOINCREMENT,
 descricao VARCHAR(255),
 inicio DATE,
 fim DATE,
@@ -17,25 +17,25 @@ idcurriculo INT, FOREIGN KEY (idcurriculo) REFERENCES curriculo(id) );
 
 CREATE TABLE formacao ( curso VARCHAR(50),
 descricao VARCHAR(50),
-id INT PRIMARY KEY,
+id INTEGER PRIMARY KEY AUTOINCREMENT,
 inicio DATE,
 fim DATE,
 idcurriculo INT,
 FOREIGN KEY (idcurriculo) REFERENCES curriculo(id) );
 
-CREATE TABLE realizacao ( id INT PRIMARY KEY,
+CREATE TABLE realizacao ( id INTEGER PRIMARY KEY AUTOINCREMENT,
 nome VARCHAR(50),
 ano DATE,
 descricao VARCHAR(255),
 idcurriculo INT, FOREIGN KEY (idcurriculo) REFERENCES curriculo(id) );
 
-CREATE TABLE habilidade ( nome VARCHAR(50), id INT PRIMARY KEY,
+CREATE TABLE habilidade ( nome VARCHAR(50), id INTEGER PRIMARY KEY AUTOINCREMENT,
 atual INT,
 maxima INT,
 idcurriculo INT, FOREIGN KEY (idcurriculo) REFERENCES curriculo(id) );
 
 CREATE TABLE personalidade ( nome VARCHAR(50),
-id INT PRIMARY KEY,
+id INTEGER PRIMARY KEY AUTOINCREMENT,
 atual INT,
 maxima INT,
 idcurriculo INT, FOREIGN KEY (idcurriculo) REFERENCES curriculo(id) );
